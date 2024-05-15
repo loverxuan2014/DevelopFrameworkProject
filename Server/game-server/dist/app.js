f9e47be2e568e7552887ad6946242da9ba3e704e
loverxuan2014 <loverxuan@foxmail.com>
2024-05-15 10:36:58 +0800

    【debug】【develop】【server】 log: update server

---
+++ Server/game-server/dist/app.js
@@ -0,0 +1,27 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const pinus_1 = require("pinus");
+const preload_1 = require("./preload");
+/**
+ *  替换全局Promise
+ *  自动解析sourcemap
+ *  捕获全局错误
+ */
+(0, preload_1.preload)();
+/**
+ * Init app for client.
+ */
+let app = pinus_1.pinus.createApp();
+app.set('name', 'Server');
+// app configuration
+app.configure('production|development', 'connector', function () {
+    app.set('connectorConfig', {
+        connector: pinus_1.pinus.connectors.hybridconnector,
+        heartbeat: 3,
+        useDict: true,
+        useProtobuf: true
+    });
+});
+// start app
+app.start();
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYXBwLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiLi4vYXBwLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7O0FBQUEsaUNBQThCO0FBQzlCLHVDQUFvQztBQUVwQzs7OztHQUlHO0FBQ0gsSUFBQSxpQkFBTyxHQUFFLENBQUM7QUFFVjs7R0FFRztBQUNILElBQUksR0FBRyxHQUFHLGFBQUssQ0FBQyxTQUFTLEVBQUUsQ0FBQztBQUM1QixHQUFHLENBQUMsR0FBRyxDQUFDLE1BQU0sRUFBRSxRQUFRLENBQUMsQ0FBQztBQUUxQixvQkFBb0I7QUFDcEIsR0FBRyxDQUFDLFNBQVMsQ0FBQyx3QkFBd0IsRUFBRSxXQUFXLEVBQUU7SUFDakQsR0FBRyxDQUFDLEdBQUcsQ0FBQyxpQkFBaUIsRUFDckI7UUFDSSxTQUFTLEVBQUUsYUFBSyxDQUFDLFVBQVUsQ0FBQyxlQUFlO1FBQzNDLFNBQVMsRUFBRSxDQUFDO1FBQ1osT0FBTyxFQUFFLElBQUk7UUFDYixXQUFXLEVBQUUsSUFBSTtLQUNwQixDQUFDLENBQUM7QUFDWCxDQUFDLENBQUMsQ0FBQztBQUVILFlBQVk7QUFDWixHQUFHLENBQUMsS0FBSyxFQUFFLENBQUMifQ==
